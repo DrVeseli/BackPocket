8d845715f8874f71fc6267b60ed586355ab6ca5a
DrVeseli <131158066+DrVeseli@users.noreply.github.com>
2024-03-19 16:02:47 +0100

    SSH and domain optinos

--- main.go
+++ main.go
@@ -7,30 +7,75 @@ import (
 	"strings"
 	"time"
 
+	"github.com/schollz/progressbar/v3"
 	"golang.org/x/crypto/ssh"
 )
 
 func main() {
+
+	// style := lipgloss.NewStyle().
+    //     Foreground(lipgloss.Color("205")).
+    //     Background(lipgloss.Color("15")).
+    //     Padding(1).
+    //     MarginRight(2).
+    //     Border(lipgloss.RoundedBorder()).
+    //     BorderForeground(lipgloss.Color("205"))
 	// Prompt user for server details
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Enter server IP address: ")
+
+	fmt.Print("Please input the IP of your VPS: ")
 	serverIP, _ := reader.ReadString('\n')
 	serverIP = strings.TrimSpace(serverIP)
+	domain := serverIP
 
 	fmt.Print("Enter username: ")
 	username, _ := reader.ReadString('\n')
 	username = strings.TrimSpace(username)
 
-	fmt.Print("Enter password: ")
-	password, _ := reader.ReadString('\n')
-	password = strings.TrimSpace(password)
+	fmt.Print("Enter password or path to SSH key: ")
+	passwordOrKeyPath, _ := reader.ReadString('\n')
+	passwordOrKeyPath = strings.TrimSpace(passwordOrKeyPath)
+
+	fmt.Print("Have you pointed a domain to the server (must if you want SSL) (y/n): ")
+	domainExist, _ := reader.ReadString('\n')
+	domainExist = strings.TrimSpace(domainExist)
+
+	if domainExist == "y" {
+		fmt.Print("Enter the domain name: ")
+		domainName, _ := reader.ReadString('\n')
+		domainName = strings.TrimSpace(domainName)
+		domain = domainName
+	}
+
+	
+	
+
+	var authMethod ssh.AuthMethod
+	if strings.HasPrefix(passwordOrKeyPath, "/") {
+		// Use SSH key
+		key, err := os.ReadFile(passwordOrKeyPath)
+		if err != nil {
+			fmt.Println("Failed to read SSH key:", err)
+			return
+		}
+		signer, err := ssh.ParsePrivateKey(key)
+		if err != nil {
+			fmt.Println("Failed to parse SSH key:", err)
+			return
+		}
+		authMethod = ssh.PublicKeys(signer)
+	} else {
+		// Use password
+		password := strings.TrimSpace(passwordOrKeyPath)
+		authMethod = ssh.Password(password)
+	}
 
 	// SSH client configuration
 	config := &ssh.ClientConfig{
 		User: username,
 		Auth: []ssh.AuthMethod{
-			ssh.Password(password),
+			authMethod,
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
@@ -42,10 +87,10 @@ func main() {
 		return
 	}
 	defer client.Close()
+	bar := progressbar.Default(7)
 
 	// Execute commands sequentially
 	commands := []string{
-		//"sudo apt-get install nginx -y",
 		"sudo apt-get install unzip -y",
 		"mkdir /var/www && mkdir /var/www/pocketbase && cd /var/www/pocketbase && wget https://github.com/pocketbase/pocketbase/releases/download/v0.22.4/pocketbase_0.22.4_linux_amd64.zip && unzip pocketbase_0.22.4_linux_amd64.zip && rm pocketbase_0.22.4_linux_amd64.zip",
 		"cd /lib/systemd/system && touch pocketbase.service",
@@ -61,7 +106,7 @@ func main() {
 		RestartSec     = 5s
 		StandardOutput = append:/var/www/pocketbase/errors.log
 		StandardError  = append:/var/www/pocketbase/errors.log
-		ExecStart      = /var/www/pocketbase/pocketbase serve --http ` + serverIP + `:80
+		ExecStart      = /var/www/pocketbase/pocketbase serve --http ` + domain + `:80
 		
 		[Install]
 		WantedBy = multi-user.target' > /lib/systemd/system/pocketbase.service`,
@@ -69,31 +114,7 @@ func main() {
 
 		"sudo systemctl enable pocketbase",
 		"sudo systemctl start pocketbase",
-		//"cd /etc/nginx/sites-available && touch pocketbase",
-		// `echo 'server {
-		// 	      listen 80;
-		// 	      server_name ` + serverIP + `;
-		// 	      client_max_body_size 10M;
-
-		// 	      location / {
-		// 	              # check http://nginx.org/en/docs/http/ngx_http_upstream_module.html#keepalive
-		// 	              proxy_set_header Connection '';
-		// 	              proxy_http_version 1.1;
-		// 	              proxy_read_timeout 360s;
-
-		// 	              proxy_set_header Host \$host;
-		// 	              proxy_set_header X-Real-IP \$remote_addr;
-		// 	             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-		// 	              proxy_set_header X-Forwarded-Proto \$scheme;
-
-		// 	              # enable if you are serving under a subpath location
-		// 	              # rewrite /yourSubpath/(.*) /\$1  break;
-
-		// 	              proxy_pass http://127.0.0.1:8090;
-		// 	      }
-		// }' >> pocketbase.service`,
-		// "sudo ln -s /etc/nginx/sites-available/pocketbase /etc/nginx/sites-enabled",
-		// "sudo systemctl restart nginx",
+		
 	}
 
 	for _, cmd := range commands {
@@ -116,11 +137,14 @@ func main() {
 			return
 		}
 		fmt.Printf("Command \"%s\" executed successfully.\n", cmd)
-		time.Sleep(5 * time.Second)
+		time.Sleep(1 * time.Second)
+		bar.Add(1)
+
 	}
 
 	fmt.Println("All commands executed successfully.")
 
 	// print made a directory for pocketbase
-	fmt.Println("You are all set, go to http://` + serverIP + `/_. Its safe to close the terminal .")
+	fmt.Println("You are all set, go to http://" + domain + "/_. It's safe to close the terminal.")
+	
 }
